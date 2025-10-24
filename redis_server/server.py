7737da18db7a4626784e956a33e865ea042d314f
shariarhossain17 <shariarhossain23@gmail.com>
2025-10-24 14:47:06 +0600

    add set function for set data

--- redis_server/server.py
+++ redis_server/server.py
@@ -6,3 +6,4 @@ from .storage import Storage
 class RedisServer:
     def __init__(self):
         storage=Storage()
+        storage.set("ss",10)
--- redis_server/storage.py
+++ redis_server/storage.py
@@ -1,10 +1,16 @@
 class Storage:
     def __init__(self):
-        self.storage={}
+        self.data={}
         self.memory=0
     
-    def set(self,key,value,expire=None):
-        print("value set")
+    def set(self,key,value,expireTime=None):
+        if key in self.data(key):
+            old_val,_,_=self.data[key]
+            self.memory-=self._calculate_memory_usage(key,old_val)
+        data_type=self._get_data_type(value)
+        self.data[key]=(value,data_type,expireTime)
+        self.memory+=self._calculate_memory_usage(key,value)
+
 
     #get data type
     def _get_data_type(self,value):
@@ -20,3 +26,9 @@ class Storage:
             return "hash"
         else:
             return "string"
+        
+    #calculate memory
+    def _calculate_memory_usage(self,key,value):
+        key_size=len(str(key).encode("utf-8"))
+        value_size=len(str(key).encode("utf-8"))
+        return key_size+value_size+64
