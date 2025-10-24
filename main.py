240fddde84fbb0d45e6bf729f2b3e97bfc14515f
shariarhossain17 <shariarhossain23@gmail.com>
2025-10-24 14:04:00 +0600

    basic setup

---
+++ main.py
@@ -0,0 +1,13 @@
+
+
+
+from redis_server import RedisServer
+
+
+def main():
+    server=RedisServer()
+
+
+
+if __name__=="__main__":
+    main()
---
+++ redis_server/server.py
@@ -0,0 +1,3 @@
+class RedisServer:
+    def __init__(self):
+        print("hello from redis server")
